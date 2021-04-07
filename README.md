ad5d5bd4967a73c0b76b859801c4060d76c81728
metacolon <timo@teemze.de>
2021-04-07 19:54:51 +0200

    Document stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RipExcel
 I was annoyed with excels options to plot stuff.
+
+## How to use
+The first argument should point to the data, the other parameters should be in the following format:
+
+    <type>=<index>
+
+It always assumes the first row to be made of labels.
+The type is either "axis" (for the axis labels) or "data" (for the data).
+The indices are obviously zero-based.
+
+Example parameters:
+
+    data.csv axis=0 data=1 data=2 data=3
--- src/de/teemze/Main.java
+++ src/de/teemze/Main.java
@@ -2,6 +2,20 @@ package de.teemze;
 
 public class Main {
 
+    /**
+     * The main entry point. The first argument should point to the data, the other parameters should be in the following format:
+     * <br/>
+     * &lt;type&gt;=&lt;index&gt;
+     * <br/>
+     * It always assumes the first row to be made of labels.
+     * The type is either "axis" (for the axis labels) or "data" (for the data).
+     * The indices are obviously zero-based.
+     * <br/>
+     * Example parameters:
+     * <br/>
+     * data.csv axis=0 data=1 data=2 data=3
+     * @param args The parameters in the format as specified above.
+     */
     public static void main(String[] args) {
         // write your code here
     }
