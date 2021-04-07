5cf815adbf3628ef265bdb6a77ef4e555ebd1222
metacolon <timo@teemze.de>
2021-04-07 20:43:13 +0200

    Cleanup of Numericanvas

--- src/de/teemze/Numericanvas.java
+++ src/de/teemze/Numericanvas.java
@@ -11,18 +11,11 @@ import java.util.List;
  */
 public class Numericanvas extends JPanel
 {
-    private List<List<Coordinate>> coordinates = new ArrayList<>(2);
+    private List<List<Coordinate>> coordinates = new ArrayList<>(0);
     List<Color> colors = Arrays.asList(Color.BLACK, Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA);
     private final static int padding = 1;
     private final static int labelPadding = 1;
-    private final static int pointWidth = 4;
-    private final static int numberYDivisions = 10;
-    private final static Color lineColor = new Color(44, 102, 230, 180);
-    private final static Color pointColor = new Color(100, 100, 100, 180);
-    private final static Color gridColor = new Color(200, 200, 200, 200);
-    private static final Stroke graphStroke = new BasicStroke(2f);
-    private List<Integer> values = new ArrayList<>(10);
-    private List<String> labels = new ArrayList<>();
+    private List<String> labels = new ArrayList<>(0);
 
     private void paintBackGround(Graphics2D g, int width, int height)
     {
@@ -44,48 +37,6 @@ public class Numericanvas extends JPanel
                 height - padding - labelPadding,
                 width - padding,
                 height - padding - labelPadding);
-    }
-
-    private void painHatchY(Graphics2D g, int width, int height, int minScore, int scoreRange, int length, FontMetrics fontMetrics)
-    {
-        final int fontHeight = fontMetrics.getHeight();
-        // create hatch marks and grid lines for y axis.
-        for (int i = 0; i < numberYDivisions + 1; i++) {
-            final int x1 = padding + labelPadding;
-            final int x2 = pointWidth + padding + labelPadding;
-            final int y = height - ((i * (height - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
-            if (length > 0) {
-                g.setColor(gridColor);
-                g.drawLine(padding + labelPadding + 1 + pointWidth, y, width - padding, y);
-                g.setColor(Color.BLACK);
-                final int tickValue = (int) (minScore + ((scoreRange * i) / numberYDivisions));
-                final String yLabel = tickValue + "";
-                final int labelWidth = fontMetrics.stringWidth(yLabel);
-                g.drawString(yLabel, x1 - labelWidth - 5, y + (fontHeight / 2) - 3);
-            }
-            g.drawLine(x1, y, x2, y);
-        }
-    }
-
-    private void paintHatchX(Graphics2D g, int width, int height, int minScore, int scoreRange, int length, FontMetrics fontMetrics)
-    {
-        final int fontHeight = fontMetrics.getHeight();
-        if (length > 1) {
-            for (int i = 0; i < length; i++) {
-                final int x = i * (width - padding * 2 - labelPadding) / (length - 1) + padding + labelPadding;
-                final int y1 = height - padding - labelPadding;
-                final int y2 = y1 - pointWidth;
-                if ((i % ((int) ((length / 20.0)) + 1)) == 0) {
-                    g.setColor(gridColor);
-                    g.drawLine(x, height - padding - labelPadding - 1 - pointWidth, x, padding);
-                    g.setColor(Color.BLACK);
-                    final String xLabel = i + "";
-                    final int labelWidth = fontMetrics.stringWidth(xLabel);
-                    g.drawString(xLabel, x - labelWidth / 2, y1 + fontHeight + 3);
-                }
-                g.drawLine(x, y1, x, y2);
-            }
-        }
     }
 
     Dimension size;
